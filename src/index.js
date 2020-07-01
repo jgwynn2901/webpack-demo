33cae7cc36cba16d635aa73552fb2eb7a98b75fa
John Gwynn <John.Gwynn@Sedgwick.com>
2020-07-01 16:03:39 -0400

    styling the editor.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import $ from "jquery";
 import "bootstrap";
-import setFontSize from "./components/setFontSize";
 const ko = require("knockout");
 const roosterjs = require("roosterjs");
 
@@ -10,6 +9,8 @@ window.ko = ko;
 
 const testData =
   "What is needed: We need the ability to format text in the areas of Select Intake Greeting, Location Lookup InstructionsEmployee Lookup Instructions, and Vehicle Lookup Instructions. At a minimum, the following formatting options are needed: Bold, Font Size, Underline, and Break (going to the next line using the enter  It would be ideal to have a formatting tool, such as the mockup below, but we can also use the same formatting techniques that are available to us in View Logic.";
+
+const checked = 'checked';
 
 export default function start() {
   console.log("y'all ready for this!");
@@ -31,15 +32,12 @@ $(document).ready(function() {
       switch (targetButton.id) {
         case "bold":
           roosterjs.toggleBold(editor);
-          toggleChecked(targetButton, roosterjs.getFormatState(editor).isBold);
           break;
         case "italic":
           roosterjs.toggleItalic(editor);
-          toggleChecked(targetButton, roosterjs.getFormatState(editor).isItalic);
           break;
         case "underline":
           roosterjs.toggleUnderline(editor);
-          toggleChecked(targetButton, roosterjs.getFormatState(editor).isUnderline);
           break;
         case "font_up":
           onSetFont(0);
@@ -48,8 +46,10 @@ $(document).ready(function() {
           onSetFont(1);
           break;
       }
-    }
+    }updateButtons();
   });
+
+  
 
   function onSetFont(direction) {
     roosterjs.changeFontSize(editor, direction);
@@ -58,9 +58,9 @@ $(document).ready(function() {
   function model(inst) {
     var self = this;
     self.LocationInstruction = ko.observable(inst);
-    self.Greeting = ko.observable("Greeting");
-    self.EmployeeInstruction = ko.observable("Employee Instruction");
-    self.VehicleInstruction = ko.observable("Vehicle Instruction");
+    self.Greeting = ko.observable("<em>Greeting</em>");
+    self.EmployeeInstruction = ko.observable("<b>Employee</b> Instruction");
+    self.VehicleInstruction = ko.observable("<i>Vehicle</i> Instruction");
     self.target = "";
     self.updateInstruction = function(text) {
         switch (self.target.id) {
@@ -81,21 +81,30 @@ $(document).ready(function() {
   }
 
   function toggleChecked(button, isChecked) {
-    let $b = $(button);
-    const checked = 'checked';
+    let $b = $("#" + button);
+    console.log(button, isChecked);
     if (isChecked) {
       if(!$b.hasClass(checked)) {
-        $b.addClass("checked");
+        $b.addClass(checked);
       }
     } else {
-      if(!$b.hasClass(checked)) {
-        $b.removeClass("checked");
-      }
+        $b.removeClass(checked);
     }
   }
 
   var viewModel = new model(testData);
   ko.applyBindings(viewModel);
+
+  function updateButtons () {
+    roosterjs.getFormatState(editor).isBold ?
+      $('#bold').addClass(checked) : $('#bold').removeClass(checked);
+
+    roosterjs.getFormatState(editor).isUnderline ?
+      $('#underline').addClass(checked) : $('#underline').removeClass(checked);
+      
+    roosterjs.getFormatState(editor).isItalic ?
+      $('#italic').addClass(checked) : $('#italic').removeClass(checked);
+  }
 
   function getContent(id) {
     switch(id) {
@@ -110,11 +119,27 @@ $(document).ready(function() {
     }
   }
 
+  function setContent(id, data) {
+    switch(id) {
+      case "greeting-tab":
+        viewModel.Greeting(data);
+        break;
+      case "location-tab":
+        viewModel.LocationInstruction(data);
+        break;  
+      case "employee-tab":
+        viewModel.EmployeeInstruction(data);
+        break;
+      case "vehicle-tab":
+        viewModel.VehicleInstruction(data);
+        break;  
+    }
+  }
+
   $("#nav-tabs").on("click", function (e) {
     e.preventDefault();
     if (e.target != e.currentTarget) {
       viewModel.target = e.target.id;
-      console.log(e.target.id);
       editor.setContent(getContent(e.target.id));
       $("#formatTexteModal").modal("show");
     }
@@ -122,7 +147,8 @@ $(document).ready(function() {
   
   $("#saveButton").on("click", (e) => {
     e.preventDefault();
-    viewModel.updateInstruction(editorDiv.innerHTML);
+    console.log('saving to ' + viewModel.target);
+    setContent(viewModel.target, editorDiv.innerHTML);
     $("#formatTexteModal").modal("hide");
   });
   start();
